84572f8f4d93455869bdddd1437007f1c24d5db4
Liu ZIxu <398488080@qq.com>
2022-05-10 16:48:34 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
 <!-- 路由出口 -->
   <router-view/>
-  <h1>news</h1>
+  <h1>news123</h1>
+  <div>
+    <i class="newsm newsm-edit-add"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+  <van-cell-group>
+    <van-cell title="单元格" value="内容" />
+    <van-cell title="单元格" value="内容" label="描述信息" />
+  </van-cell-group>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import router from './router'
 import store from './store'
 import './styles/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+// 加载vant核心组件库
+import Vant from 'vant'
+
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+// 注册使用vant组件库
+createApp(App).use(store).use(router).use(Vant).mount('#app')
